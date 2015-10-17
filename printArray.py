6a8420c5167316969f2ed851371332148bcf6122
Michael Hoppes <hoppesm@oregonstate.edu>
2015-10-17 14:02:05 -0700

    Add write to file capability with writeFile; Syntax cleanup

--- printArray.py
+++ printArray.py
@@ -1,27 +1,27 @@
-
+# Prints array from start to stop
 def printArray(array, start, stop):
     print len(array)
     if stop >= len(array):
         stop = len(array)
     print array[start:stop]
 
-#read a file
+# read a file
 # convert each line to an array or integers
 # returns an array of arrays of integers
-#@params loc = location, name = file name
+# @params loc = location, name = file name
 
 def read_to_array(loc, name):
     result = []
-    f = open(loc+name, 'r')
+    f = open(loc + name, 'r')
     for line in f:
         # get rid of junk
         # for some reason makes me use equal...not sure that's right but works
-        line=  line.replace('[', '')
+        line = line.replace('[', '')
         line = line.replace(']', '')
         line = line.replace('\n', '')
         line = line.split(',')
 
-        #convert each element to an integer
+        # convert each element to an integer
         newline = []
         for i in line:
             v = int(i)
@@ -29,3 +29,15 @@ def read_to_array(loc, name):
         result.append(newline)
 
     return result
+
+# Output to a file, the contents specified, defaulting to appending
+def writeFile(loc, name, contents, method='a'):
+    try:
+        f = open(loc + name, method)
+    except:
+        print "The file could not be opened: ", sys.exc_info()[0]
+
+    f.write(contents)
+    f.write('\n')
+    f.close()
+
