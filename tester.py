82d616ab75bffa059d812479e95f9013a5f8c2dd
Michael Hoppes <hoppesm@oregonstate.edu>
2015-10-26 18:37:43 -0700

    Move reports to directory, change filename format, P5 DP and Greedy runs re-calculated

--- tester.py
+++ tester.py
@@ -2,6 +2,8 @@
 
 import cs325_p2, io, time
 
+dir = './reports/'
+
 # Part 4
 # for A in range(2000, 2201, 10):
 #    V = [1,5,10,25,50]
@@ -25,109 +27,104 @@ import cs325_p2, io, time
 #    print 'changedb for A:',A,'m =',m,' time:',stop-start
 
 
+#######################################################################################################################
 # Part 5
-# io.writeFile('', 'p5v1changeslow2k.txt', 'changeslow, A [2000, 2200], V1=[1,2,6,12,24,48,60]', 'w')
-# for A in range(2000, 2201):
-#    V1 = [1,2,6,12,24,48,60]
-#    start = time.time()
-#    C,m = cs325_p2.changeSlow(A, V1)
-#    stop = time.time()
-#    print 'changeslow for V1,A:',A,'m =',m
-#    io.writeFile('', 'p5v1changeslow2k.txt', str(m))
-
-# io.writeFile('', 'p5v2changeslow2k.txt', 'changeslow, A [2000, 2200], V2=[1,6,13,37,15]', 'w')
-# for A in range(2000, 2201):
-#    V2 = [1,6,13,37,150]
-#    start = time.time()
-#    C,m = cs325_p2.changeSlow(A, V2)
-#    stop = time.time()
-#    print 'changeslow for V2,A:',A,'m =',m
-#    io.writeFile('', 'p5v2changeslow2k.txt', str(m))
-
-# io.writeFile('', 'p5v1changegreedy2k.txt', 'changegreedy, A [2000, 2200], V1=[1,2,6,12,24,48,60]', 'w')
-# for A in range(2000, 2201):
-#     V1 = [1,2,6,12,24,48,60]
-#     start = time.time()
-#     C,m = cs325_p2.changegreedy(A, V1)
-#     stop = time.time()
-#     io.writeFile('', 'p5v1changegreedy2k.txt', str(m) + ',' + str(stop-start))
-#
-# io.writeFile('', 'p5v2changegreedy2k.txt', 'changegreedy, A [2000, 2200], V2=[1,6,13,37,15]', 'w')
-# for A in range(2000, 2201):
-#     V2 = [1,6,13,37,150]
-#     start = time.time()
-#     C,m = cs325_p2.changegreedy(A, V2)
-#     stop = time.time()
-#     io.writeFile('', 'p5v2changegreedy2k.txt', str(m) + ',' + str(stop-start))
-#
-# io.writeFile('', 'p5v1changedb2k.txt', 'changedb, A [2000, 2200], V1=[1,2,6,12,24,48,60]', 'w')
-# for A in range(2000, 2201):
-#     V1 = [1,2,6,12,24,48,60]
-#     start = time.time()
-#     m = cs325_p2.changedp(A, V1)
-#     stop = time.time()
-#     io.writeFile('', 'p5v1changedb2k.txt', str(m) + ',' + str(stop-start))
-#
-# io.writeFile('', 'p5v2changedb2k.txt', 'changedb, A [2000, 2200], V2=[1,6,13,37,15]', 'w')
-# for A in range(2000, 2201):
-#     V2 = [1,6,13,37,150]
-#     start = time.time()
-#     m = cs325_p2.changedp(A, V2)
-#     stop = time.time()
-#     io.writeFile('', 'p5v2changedb2k.txt', str(m) + ',' + str(stop-start))
-#
-# io.writeFile('', 'p5v1changegreedy10k.txt', 'changegreedy, A [10000, 10200], V1=[1,2,6,12,24,48,60]', 'w')
-# for A in range(10000, 10201):
-#     V1 = [1,2,6,12,24,48,60]
-#     start = time.time()
-#     C,m = cs325_p2.changegreedy(A, V1)
-#     stop = time.time()
-#     io.writeFile('', 'p5v1changegreedy10k.txt', str(m) + ',' + str(stop-start))
-#
-# io.writeFile('', 'p5v2changegreedy10k.txt', 'changegreedy, A [10000, 10200], V2=[1,6,13,37,150]', 'w')
-# for A in range(10000, 10201):
-#     V2 = [1,6,13,37,150]
-#     start = time.time()
-#     C,m = cs325_p2.changegreedy(A, V2)
-#     stop = time.time()
-#     io.writeFile('', 'p5v2changegreedy10k.txt', str(m) + ',' + str(stop-start))
-#
-# io.writeFile('', 'p5v1changedb10k.txt', 'changedb, A [10000, 10200], V1=[1,2,6,12,24,48,60]', 'w')
-# for A in range(10000, 10201):
-#     V1 = [1,2,6,12,24,48,60]
-#     start = time.time()
-#     m = cs325_p2.changedp(A, V1)
-#     stop = time.time()
-#     io.writeFile('', 'p5v1changedb10k.txt', str(m) + ',' + str(stop-start))
-#
-# io.writeFile('', 'p5v2changedb10k.txt', 'changedby, A [10000, 10200], V2=[1,6,13,37,150]', 'w')
-# for A in range(10000, 10201):
-#     V2 = [1,6,13,37,150]
-#     start = time.time()
-#     m = cs325_p2.changedp(A, V2)
-#     stop = time.time()
-#     io.writeFile('', 'p5v2changedb10k.txt', str(m) + ',' + str(stop-start))
-
-
-# Part 6
 Alow = 100
 Ahigh = 300
 
-io.writeFile('', 'p6changeslow2.txt', 'changeslow, A [' + str(Alow) + ', ' + str(Ahigh) + ']', 'w')
-io.writeFile('', 'p6changegreedy.txt', 'changegreedy, A [' + str(Alow) + ', ' + str(Ahigh) + ']', 'w')
-io.writeFile('', 'p6changedb.txt', 'changedb, A [' + str(Alow) + ', ' + str(Ahigh) + ']', 'w')
+AnormalLow = 10000
+AnormalHigh = 10201
+
+io.writeFile(dir, 'p5changeSlowV1Low.txt', 'change slow, A [' + str(Alow) + ', ' + str(Ahigh) + '], V1=[1,2,6,12,24,48,60]', 'w')
+io.writeFile(dir, 'p5changeSlowV2Low.txt', 'change slow, A [' + str(Alow) + ', ' + str(Ahigh) + '], V2=[1,6,13,37,15]', 'w')
+io.writeFile(dir, 'p5changeGreedyV1Low.txt', 'change greedy, A [' + str(Alow) + ', ' + str(Ahigh) + '], V1=[1,2,6,12,24,48,60]', 'w')
+io.writeFile(dir, 'p5changeGreedyV2Low.txt', 'change greedy, A [' + str(Alow) + ', ' + str(Ahigh) + '], V2=[1,6,13,37,15]', 'w')
+io.writeFile(dir, 'p5changeDPV1Low.txt', 'change dp, A [' + str(Alow) + ', ' + str(Ahigh) + '], V1=[1,2,6,12,24,48,60]', 'w')
+io.writeFile(dir, 'p5changeDPV2Low.txt', 'change dp, A [' + str(Alow) + ', ' + str(Ahigh) + '], V2=[1,6,13,37,15]', 'w')
+io.writeFile(dir, 'p5changeGreedyV1High.txt', 'change greedy, A [' + str(AnormalLow) + ', ' + str(AnormalHigh) + '], V1=[1,2,6,12,24,48,60]', 'w')
+io.writeFile(dir, 'p5changeGreedyV2High.txt', 'change greedy, A [' + str(AnormalLow) + ', ' + str(AnormalHigh) + '], V2=[1,6,13,37,150]', 'w')
+io.writeFile(dir, 'p5changeGDPV1High.txt', 'change dp, A [' + str(AnormalLow) + ', ' + str(AnormalHigh) + '], V1=[1,2,6,12,24,48,60]', 'w')
+io.writeFile(dir, 'p5changeGDPV2High.txt', 'change dp, A [' + str(AnormalLow) + ', ' + str(AnormalHigh) + '], V2=[1,6,13,37,150]', 'w')
 
 for A in range(Alow, Ahigh):
-    V = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+    # V1 = [1,2,6,12,24,48,60]
+    # start = time.time()
+    # C,m = cs325_p2.changeSlow(A, V1)
+    # stop = time.time()
+    # io.writeFile(dir, 'p5changeSlowV1Low.txt', str(m) + ',' + str(stop-start))
+    # V2 = [1,6,13,37,150]
+    # start = time.time()
+    # C,m = cs325_p2.changeSlow(A, V2)
+    # stop = time.time()
+    # io.writeFile(dir, 'p5changeSlowV2Low.txt', str(m) + ',' + str(stop-start))
+
+    V1 = [1,2,6,12,24,48,60]
     start = time.time()
-    C, m = cs325_p2.changeSlow(A, V)
+    C,m = cs325_p2.changegreedy(A, V1)
     stop = time.time()
-    io.writeFile('', 'p6changeslow.txt', str(m) + ',' + str(stop - start))
+    io.writeFile(dir, 'p5changeGreedyV1Low.txt', str(m) + ',' + str(stop-start))
+    V2 = [1,6,13,37,150]
     start = time.time()
-    C, m = cs325_p2.changegreedy(A, V)
+    C,m = cs325_p2.changegreedy(A, V2)
     stop = time.time()
-    io.writeFile('', 'p6changegreedy.txt', str(m) + ',' + str(stop - start))
+    io.writeFile(dir, 'p5changeGreedyV2Low.txt', str(m) + ',' + str(stop-start))
+
+    V1 = [1,2,6,12,24,48,60]
     start = time.time()
-    m = cs325_p2.changedp(A, V)
+    m = cs325_p2.changedp(A, V1)
     stop = time.time()
-    io.writeFile('', 'p6changedb.txt', str(m) + ',' + str(stop - start))
+    io.writeFile(dir, 'p5changeDPV1Low.txt', str(m) + ',' + str(stop-start))
+    V2 = [1,6,13,37,150]
+    start = time.time()
+    m = cs325_p2.changedp(A, V2)
+    stop = time.time()
+    io.writeFile(dir, 'p5changeDPV2Low.txt', str(m) + ',' + str(stop-start))
+
+for A in range(AnormalLow, AnormalHigh):
+    V1 = [1,2,6,12,24,48,60]
+    start = time.time()
+    C,m = cs325_p2.changegreedy(A, V1)
+    stop = time.time()
+    io.writeFile(dir, 'p5changeGreedyV1High.txt', str(m) + ',' + str(stop-start))
+
+    V2 = [1,6,13,37,150]
+    start = time.time()
+    C,m = cs325_p2.changegreedy(A, V2)
+    stop = time.time()
+    io.writeFile(dir, 'p5changeGreedyV2High.txt', str(m) + ',' + str(stop-start))
+
+    V1 = [1,2,6,12,24,48,60]
+    start = time.time()
+    m = cs325_p2.changedp(A, V1)
+    stop = time.time()
+    io.writeFile(dir, 'p5changeGDPV1High.txt', str(m) + ',' + str(stop-start))
+
+    V2 = [1,6,13,37,150]
+    start = time.time()
+    m = cs325_p2.changedp(A, V2)
+    stop = time.time()
+    io.writeFile(dir, 'p5changeGDPV2High.txt', str(m) + ',' + str(stop-start))
+
+#######################################################################################################################
+# Part 6
+# Alow = 100
+# Ahigh = 300
+#
+# io.writeFile(dir, 'p6changeslow2.txt', 'changeslow, A [' + str(Alow) + ', ' + str(Ahigh) + ']', 'w')
+# io.writeFile(dir, 'p6changegreedy.txt', 'changegreedy, A [' + str(Alow) + ', ' + str(Ahigh) + ']', 'w')
+# io.writeFile(dir, 'p6changedb.txt', 'changedb, A [' + str(Alow) + ', ' + str(Ahigh) + ']', 'w')
+#
+# for A in range(Alow, Ahigh):
+#     V = [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+#     start = time.time()
+#     C, m = cs325_p2.changeSlow(A, V)
+#     stop = time.time()
+#     io.writeFile(dir, 'p6changeslow.txt', str(m) + ',' + str(stop - start))
+#     start = time.time()
+#     C, m = cs325_p2.changegreedy(A, V)
+#     stop = time.time()
+#     io.writeFile(dir, 'p6changegreedy.txt', str(m) + ',' + str(stop - start))
+#     start = time.time()
+#     m = cs325_p2.changedp(A, V)
+#     stop = time.time()
+#     io.writeFile(dir, 'p6changedb.txt', str(m) + ',' + str(stop - start))
